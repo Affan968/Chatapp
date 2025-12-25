efd548adcfb93f600dabf6fa22333225867821b3
Affan986 <muhammadaffanjawwad2292@gmail.com>
2025-12-26 02:53:00 +0500

    chore: resolved merge conflicts and removed git markers from all files

--- config/configs.js
+++ config/configs.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
-import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, setDoc, doc, getDoc, getDocs, onSnapshot, query, where, serverTimestamp, updateDoc, deleteDoc,orderBy } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore, collection, addDoc, setDoc, doc, getDoc, getDocs, onSnapshot, query, where, serverTimestamp, updateDoc, deleteDoc, orderBy } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBM-oD81V9HIAGrRiLCQGh8lPP-UmuaidI",
   authDomain: "chataap-5522c.firebaseapp.com",
@@ -22,64 +16,30 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Exporting functions and instances
 export {
-  createUserWithEmailAndPassword,
   auth,
-  signInWithEmailAndPassword,
   db,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
   collection,
   addDoc,
   setDoc,
   doc,
-  signOut, getDoc, getDocs, onSnapshot, query, where, serverTimestamp, updateDoc, deleteDoc,
-  onAuthStateChanged,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  where,
+  serverTimestamp,
+  updateDoc,
+  deleteDoc,
   orderBy
-
-=======
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-  import { getFirestore, collection, addDoc,setDoc,doc } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBM-oD81V9HIAGrRiLCQGh8lPP-UmuaidI",
-    authDomain: "chataap-5522c.firebaseapp.com",
-    projectId: "chataap-5522c",
-    storageBucket: "chataap-5522c.firebasestorage.app",
-    messagingSenderId: "356212896262",
-    appId: "1:356212896262:web:0757ac8df51ae0c3244987",
-    measurementId: "G-K7YCE25GM0"
-  };
-
-  // Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export {
-  createUserWithEmailAndPassword,
-  auth,
-  signInWithEmailAndPassword,
-  db,
-  collection,
-  addDoc,
-  setDoc,
-  doc
-
->>>>>>> b853f791e6e776f6bee3fffade066b4ab3b30668
-}
+};
